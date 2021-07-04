217be960c3a9217898e8e7f927d19cb55ca145cf
shinshin0773 <82130318+shinshin0773@users.noreply.github.com>
2021-07-04 21:53:19 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
 /* jshint curly:true, debug:true */
 /* globals $, firebase */
 
-//Firebase Twitterログインのための
-var uiConfig = {
-  // ログイン完了時のリダイレクト先
-  signInSuccessUrl: 'done.html',
-
-  // 利用する認証機能
-  signInOptions: [
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID
-  ],
-
-  // 利用規約のURL(任意で設定)
-  // tosUrl: 'http://example.com/kiyaku/',
-  // プライバシーポリシーのURL(任意で設定)
-  // privacyPolicyUrl: 'http://example.com/privacy'
-};
-
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-ui.start('#firebaseui-auth-container', uiConfig);
 
 /**
  * -------------------
